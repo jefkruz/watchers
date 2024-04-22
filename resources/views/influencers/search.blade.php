2589fef5408ebefe14ebf9a305bea62fae79943d
jefkruz <josephugbeva@gmail.com>
2024-04-22 16:44:46 +0100

    added session comments

--- resources/views/influencers/search.blade.php
+++ resources/views/influencers/search.blade.php
@@ -3,15 +3,9 @@
 
 
 @section('content')
-    @if ($videos->count() ==0 && $posts->count() == 0)
-        <div class="alert alert-danger solid alert-dismissible fade show">
-            <svg viewBox="0 0 24 24" width="24 " height="24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round" class="me-2"><polygon points="7.86 2 16.14 2 22 7.86 22 16.14 16.14 22 7.86 22 2 16.14 2 7.86 7.86 2"></polygon><line x1="15" y1="9" x2="9" y2="15"></line><line x1="9" y1="9" x2="15" y2="15"></line></svg>
-            <strong>Sorry!</strong> No Result Found.
-            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="btn-close">
-            </button>
-        </div>
-    @else
-        @if($videos && $posts->count() == 0)
+{{--    @if ($videos->count() ==0 && $posts->count() == 0)--}}
+
+        @if($videos)
             <div class="row">
                 <h3 class="card-title mb-5">Video {{$page_title}}</h3>
 
@@ -34,7 +28,7 @@
             </div>
         @endif
 
-        @if($posts && $videos->count() == 0)
+        @if($posts )
             <div class="row">
                 <h3 class="card-title mb-5">Article {{$page_title}}</h3>
 
@@ -76,6 +70,14 @@
             @endforeach
             </div>
         @endif
-
+    @if($posts->isEmpty() && $videos->isEmpty())
+                <div class="alert alert-danger solid alert-dismissible fade show">
+                    <svg viewBox="0 0 24 24" width="24 " height="24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round" class="me-2"><polygon points="7.86 2 16.14 2 22 7.86 22 16.14 16.14 22 7.86 22 2 16.14 2 7.86 7.86 2"></polygon><line x1="15" y1="9" x2="9" y2="15"></line><line x1="9" y1="9" x2="15" y2="15"></line></svg>
+                    <strong>Sorry!</strong> No Result Found.
+                    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="btn-close">
+                    </button>
+                </div>
     @endif
+
+
 @endsection
